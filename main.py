a7e6b81551a99bf8f0a6472bc87520cab7328f54
AndrewGrabowski2024 <819788@mystma.org>
2021-09-28 14:46:11 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
 #WRITE YOUR CODE IN THIS FILE
+def greaterThan (x,y):
+    if x > y:
+        return True
+
+    else:
+        return False
+print(greaterThan(52, 2))
+def lessThan (x,y):
+    if x < y:
+        return True
+
+    else:
+        return False
+print(lessThan(52, 2))
+def equalTo (x,y):
+    if x == y:
+        return True
+
+    else:
+        return False
+print(equalTo(52, 2))
+def greaterOrEqual (x,y):
+    if x > y:
+        return True
+
+    elif x == y:
+        return True 
+
+    else:
+        return False
+print(greaterOrEqual(2, 2))
+def lessOrEqual (x,y):
+    if x < y:
+        return True
+
+    elif x == y:
+        return True 
+
+    else:
+        return False
+print(lessOrEqual(2, 2))
